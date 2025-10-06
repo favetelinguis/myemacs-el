fd2407c593c468d06c624e4ef893765a81e56dc4
Henrik Larsson <favetelinguis1@gmail.com>
2025-10-06 21:30:13 +0200

    add zig command to test filter

--- lisp/my-zig.el
+++ lisp/my-zig.el
@@ -9,6 +9,7 @@
   :bind (:map zig-mode-map
 	      ("C-c C-c t" . my/zig-build-test)
 	      ("C-c C-c l" . my/zig-test-file-local)
+	      ("C-c C-c r" . my/zig-build-test-filter-at-point)
 	      ("C-c C-c f" . my/zig-test-filter-at-point))
   :config
   (setq zig-format-on-save nil) ; rely on :editor format instead
@@ -35,7 +36,24 @@
     (if quoted-string
 	(progn
           (setq compile-command 
-                (format "zig test %s --filter %s" relative-path quoted-string))
+                (format "zig test %s --test-filter %s" relative-path quoted-string))
+	  (let  ((default-directory project-root))
+	    (compile compile-command)))
+      (message "No quoted string found at point"))))
+
+;; I can pass in multiple test-filter might want to extend this to that?
+;; zig build test -Dtest-filter="parse ls output" -Dtest-filter="next test"
+(defun my/zig-build-test-filter-at-point ()
+  "Run zig build test with filter using quoted string at point."
+  (interactive)
+  (let* ((project-root (or (project-root (project-current))
+                           default-directory))
+         (relative-path (file-relative-name (buffer-file-name) project-root))
+         (quoted-string (thing-at-point 'string t)))
+    (if quoted-string
+	(progn
+          (setq compile-command 
+                (format "zig build test --summary new -Dtest-filter=%s" quoted-string))
 	  (let  ((default-directory project-root))
 	    (compile compile-command)))
       (message "No quoted string found at point"))))
