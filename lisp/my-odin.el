5f26e288eeb9512706b4bb279877252c9c0ea0da
Henrik Larsson <favetelinguis1@gmail.com>
2025-11-02 21:03:05 +0100

    improve odin test

--- lisp/my-odin.el
+++ lisp/my-odin.el
@@ -1,9 +1,23 @@
 ;;; my-odin.el --- A simple hello world package
 ;;; Commentary:
 ;; A basic example package
+
+(defun odin-test-at-point ()
+  "Run odin test command with current buffer's folder. If word under cursor exists, add it as test name."
+  (interactive)
+  (let ((word (thing-at-point 'word t))
+        (folder (file-name-directory (buffer-file-name))))
+    (if folder
+        (let ((command (if word
+                           (format "odin test %s -define:ODIN_TEST_NAMES=%s" folder word)
+                         (format "odin test %s" folder))))
+          (compile command))
+      (message "Buffer is not associated with a file"))))
+
 (use-package odin-mode
   :vc (:url "https://github.com/mattt-b/odin-mode" :rev :newest)
   :after eglot
+  :bind (("C-c t" . odin-test-at-point))
   :hook ((odin-mode) . eglot-ensure)
   ((odin-mode) . (lambda ()
 		   (setq tab-width 4
